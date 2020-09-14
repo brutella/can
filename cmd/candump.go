c7049f083b97b70a4340ea2461750add3f7d787d
Jesper Vesterberg <jumziey@gmail.com>
2020-09-14 12:25:01 +0200

    Changed candump to use dockstavarvet package instead (seems brutella does not maintain can).

--- cmd/candump.go
+++ cmd/candump.go
@@ -6,11 +6,12 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/brutella/can"
 	"log"
 	"net"
 	"os"
 	"os/signal"
+
+	"github.com/dockstavarvet/can"
 )
 
 var i = flag.String("if", "", "network interface name")
